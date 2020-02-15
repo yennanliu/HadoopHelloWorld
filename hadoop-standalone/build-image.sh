7892dc488c639a9adfbb348a0d5ea251ae3071db
yennanliu <f339339@gmail.com>
2020-02-15 11:54:38 +0800

    add hadoop-standalone/

---
+++ hadoop-standalone/build-image.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo ""
+sudo docker build -t yennanliu/hadoop-standalone:latest .
+echo -e "\nBuilding docker image is done\n"
+echo ""
---
+++ hadoop-standalone/start-container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# start hadoop standalone container
+sudo docker rm -f hadoop-standalone &> /dev/null
+echo "start hadoop-standalone container..."
+
+# volume dir
+mkdir ~/src/
+sudo chmod -R 777 ~/src
+sudo docker run -itd \
+				-v ~/src:/root/src \
+				--name hadoop-standalone \
+                yennanliu/hadoop-standalone &> /dev/null
+
+# get into hadoop-hbase container
+sudo docker exec -it hadoop-standalone bash
