dd3bb164dcb78373a996a4046ac52737120cd1b9
yennanliu <f339339@gmail.com>
2020-02-18 11:54:43 +0800

    add hadoop-pig/quick_start.sh and README

---
+++ hadoop-pig/README.md
@@ -0,0 +1,6 @@
+## Hadoop-Pig
+
+## Ref 
+- https://hub.docker.com/r/ipedrazas/hadoop-pig/dockerfile
+- https://github.com/ipedrazas/docker-hadoop-2.5.2-pig-0.14.0
+- https://registry.hub.docker.com/u/sequenceiq/hadoop-docker/
---
+++ hadoop-pig/quick_start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone https://github.com/yennanliu/HadoopHelloWorld.git
+docker pull ipedrazas/hadoop-pig
+docker run  --mount \
+type=bind,\
+source="$(pwd)"/.,\
+target=/HadoopHelloWorld \
+-i -t ipedrazas/hadoop-pig /etc/bootstrap.sh -bash
