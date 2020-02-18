e40a841dd9763198dea47c2e284c2a948e58e9ed
yennanliu <f339339@gmail.com>
2020-02-18 12:29:14 +0800

    update  hadoop-pig/quick_start.sh

--- hadoop-pig/quick_start.sh
+++ hadoop-pig/quick_start.sh
@@ -6,4 +6,4 @@ docker run  --mount \
 type=bind,\
 source="$(pwd)"/.,\
 target=/HadoopHelloWorld \
--i -t ipedrazas/hadoop-pig /etc/bootstrap.sh -bash
+-i -t ipedrazas/hadoop-pig /etc/bootstrap.sh -bash -c "mv HadoopHelloWorld/HadoopHelloWorld/data data && pig -x local"
