dc092b5bca2fa13aa4023aeac657b4e58143cec1
yennanliu <f339339@gmail.com>
2020-02-18 12:48:17 +0800

    fix command to make all process automate (quick_start.sh)

--- hadoop-pig/quick_start.sh
+++ hadoop-pig/quick_start.sh
@@ -6,6 +6,4 @@ docker run  --mount \
 type=bind,\
 source="$(pwd)"/.,\
 target=/HadoopHelloWorld \
--i -t ipedrazas/hadoop-pig /etc/bootstrap.sh -bash -c "mv HadoopHelloWorld/HadoopHelloWorld/data data && pig -x local"
-
-# to fix : how to auto copy data and bootstrap.sh then access pig console
+-i -t ipedrazas/hadoop-pig /bin/bash -c " ./etc/bootstrap.sh && mv HadoopHelloWorld/HadoopHelloWorld/data data && pig -x local"
