bc59e09ffe5920bdf4e9df1ec100d58d7e8bf238
yennanliu <f339339@gmail.com>
2020-02-22 18:08:14 +0800

    update bash script, xml,.. files for Hadoop ec2 init config

--- hadoop-ec2/install_hadoop_V2.sh
+++ hadoop-ec2/install_hadoop_V2.sh
@@ -1,19 +1,78 @@
 #!/bin/sh
 
+# 1) declare env var
 export HADOOP_VERSION=hadoop-2.8.2
 
-
+# 2) prerequisite
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
 sudo apt-get install default-jdk -y 
+
+# 3) install hadoop 
+mkdir hadoopdata
 wget https://archive.apache.org/dist/hadoop/core/${HADOOP_VERSION}/${HADOOP_VERSION}.tar.gz
 sudo tar xzf ${HADOOP_VERSION}.tar.gz 
 sudo mv ${HADOOP_VERSION} /usr/local/hadoop
 sudo chown -R ubuntu /usr/local/hadoop/
 
-# Hadoop configuration
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64" >>  ~/.profile
-echo "export PATH={"PATH:JAVA_HOME/bin"}" >>  ~/.profile
-echo "export HADOOP_HOME=/usr/local/hadoop"  >> ~/.profile 
-echo "export PATH=PATH:HADOOP_HOME/bin"  >> ~/.profile
+export HADOOP_HOME=/home/ubuntu/{HADOOP_VERSION}
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib"
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
 
-# hadoop-env.sh
-echo "export JAVA_HOME=${JAVA_HOME}" >> /usr/local/hadoop/etc/hadoop/hadoop-env.sh
+# 4) update ~/.bashrc
+echo "export HADOOP_HOME=/home/ubuntu/{HADOOP_VERSION}"  >> ~/.bashrc
+echo "export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native"  >> ~/.bashrc
+echo "export HADOOP_OPTS={"-Djava.library.path=$HADOOP_HOME/lib"}"  >> ~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle"  >> ~/.bashrc
+echo "PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin"  >> ~/.bashrc
+
+source ~/.bashrc
+
+# 5) Setting hadoop environment for password less ssh access
+ssh-keygen -t rsa -P ''
+cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+
+# 6) update sshd_config 
+# /etc/ssh/sshd_config
+# PasswordAuthentication yes
+
+# 7) restart bash
+sudo service ssh restart
+ssh localhost
+exit
+
+# 8) git clone code 
+git clone https://github.com/yennanliu/HadoopHelloWorld.git
+
+# 8) update core-site.xml
+mv HadoopHelloWorld/hadoop-ec2/core-site.xml /home/ubuntu/hadoop-2.7.3/etc/hadoop/
+
+# 9) update hadoop-env.sh
+mv HadoopHelloWorld/hadoop-ec2/hadoop-env.sh /home/ubuntu/hadoop-2.7.3/etc/hadoop/
+
+# 10) update yarn-site.xml
+mv HadoopHelloWorld/hadoop-ec2/yarn-site.xml /home/ubuntu/hadoop-2.7.3/etc/hadoop/
+
+# 11) update hdfs-site.xml
+mv HadoopHelloWorld/hadoop-ec2/hdfs-site.xml /home/ubuntu/hadoop-2.7.3/etc/hadoop/
+
+# 12) update mapred-site.xml
+mv HadoopHelloWorld/hadoop-ec2/mapred-site.xml /home/ubuntu/hadoop-2.7.3/etc/hadoop/
+
+# 13) format HDFS (init)
+hdfs namenode -format
+
+# 14) start dfs, yarn
+bash /home/ubuntu/{HADOOP_VERSION}/sbin/start-dfs.sh
+bash /home/ubuntu/{HADOOP_VERSION}/sbin/start-yarn.sh
+
+# 15) check hadoop processes /daemons running on hadoop
+jps 
+
+# 15) stop dfs, yarn
+bash /home/ubuntu/{HADOOP_VERSION}/sbin/start-dfs.sh
+bash /home/ubuntu/{HADOOP_VERSION}/sbin/start-yarn.sh
