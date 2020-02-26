e554e9727242e83248c04fcf3ba20b93adfd6b7c
yennanliu <f339339@gmail.com>
2020-02-26 15:13:26 +0800

    add script mrjob_wordcount.py

---
+++ hadoophelloworld/mrjob_wordcount.py
@@ -0,0 +1,36 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re 
+
+WORD_RE = re.compile(r"[\w']+")
+
+class MRWordCount(MRJob):
+
+    def steps(self):
+        return [ 
+            MRStep(mapper = self.mapper_get_words,
+                   combiner = self.combiner_count_words,
+                   reducer = self.reducer_count_words),
+            #MRStep(reducer = self.reducer_find_max_word)        
+            ]
+
+    def mapper_get_words(self, _, line):
+        # yield each word in the line 
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+
+    def combiner_count_words(self, word, counts):
+        # optimization : sum the words seen so far 
+        yield (word, sum(counts))
+
+    def reducer_count_words(self, word, counts):
+        # send all (num_occurrences, word) pairs to the same reducer
+        # num_occurrences, then we can use max() function 
+        yield None, (sum(counts), word)
+
+    def reducer_find_max_word(self, _, word_count_pairs):
+        yield max(word_count_pairs)
+
+
+if __name__ == '__main__':
+    MRWordCount().run()
